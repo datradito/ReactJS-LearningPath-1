2b6b63e709bd1aede3ca5e8d0387f41834dcb9f3
Fernandez Suarez, Martina <m.b.fernandez.suarez@>
2022-06-21 15:25:33 -0300

    starting test for counterApp

---
+++ src/test-base/counterApp.jsx
@@ -0,0 +1,21 @@
+export default function Counter() {
+    const [click, setClick] = useState(0);
+    const handleAddClick = () => {
+        setClick(click + 1)
+    }
+    const handleRestartCounter = () => {
+        setClick(0);
+    }
+    return (
+        <>
+            <section>
+                <h2>Contador de clicks</h2>
+                <div>
+                    <p>Usted ha clickeado el botón: {click} veces</p>
+                    <button onClick={handleAddClick}>Click aquí</button>
+                    <button onClick={handleRestartCounter}>Restaurar contador</button>
+                </div>
+            </section>
+        </>
+    )
+}
