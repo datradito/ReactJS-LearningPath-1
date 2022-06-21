42ffb0023429580208e60af3d6aeb116d514df77
Fernandez Suarez, Martina <m.b.fernandez.suarez@>
2022-06-21 13:18:15 -0300

    Initiating test features

---
+++ tests/demo.test.js
@@ -0,0 +1,10 @@
+describe('Pruebas en el componente <Demo>', ()=> {//No es necesario escribir este describe, sólo añade un subtitulo a los test
+
+    test('Título de la prueba', () => {
+    //Inicialización
+    //Estímulo
+    //Obervar comportamiento
+    /*expect( message1 ).toBe( message2 ) //Espero que el mensaje1 sea igual a mensaje2*/
+    });
+    
+});
