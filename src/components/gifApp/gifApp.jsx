469ccfab6f2f68fdfd6a84b2993d21229c04d319
Fernandez Suarez, Martina <m.b.fernandez.suarez@>
2022-06-22 13:16:01 -0300

    Styles

--- src/components/gifApp/gifApp.jsx
+++ src/components/gifApp/gifApp.jsx
@@ -4,8 +4,8 @@ export default function GifApp() {
     return (
         <>
             <section>
-                <h1>Trabajo de la sección 6</h1>
-                <h2>Gif browser</h2>
+                <h2>Trabajo de la sección 6</h2>
+                <h3>Gif browser</h3>
             </section>
         </>
     )
