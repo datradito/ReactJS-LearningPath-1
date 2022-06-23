977623e259024cddc4964ca29f0b9d40136d2fa7
Paloma <lourdespalomaherrera@gmail.com>
2022-06-23 01:30:46 -0300

    modulo 1-4

---
+++ counter-app-vite/src/main.jsx
@@ -0,0 +1,28 @@
+//importamos react 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from "./FirstApp";
+import { CounterApp } from './CounterApp';
+
+import './styles.css';
+
+
+
+
+//creamos el root al que le vamos a agregar el texto.
+/* ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <HelloWorldApp/>
+    </React.StrictMode>
+) */
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+         <FirstApp/> 
+         <CounterApp/>
+
+    </React.StrictMode>
+)
