bee2e23545a963397e36f50b33242c11ccbfb063
Fernandez Suarez, Martina <m.b.fernandez.suarez@>
2022-06-24 17:55:29 -0300

    starting tests

--- tests/test-base/gifApp.test.jsx
+++ tests/test-base/gifApp.test.jsx
@@ -2,39 +2,18 @@
  * @jest-environment jsdom
  */
 
-/*
+
  import React from 'react';
  import { fireEvent, render, screen } from '@testing-library/react';
- import { Counter } from '../../src/test-base/counterApp';
- 
- describe('Pruebas de counterApp', () => {
+ import { GifApp } from '../../src/Gif_App/GifApp';
+
+ describe('Pruebas de GifApp', () => {
      test('Debe hacer match con el snapshot', () => {
-         const { container } = render( <Counter/> );
+         const { container } = render( <GifApp/> );
          expect( container ).toMatchSnapshot( );
      });
  
-     test('Click debe tener un valor de 0 al iniciar la app', () => {
-         //Inicialización
-         const clickFirstValue = 0;
- 
-         //Estímulo
-         render( <Counter/> );
-         
-         //Obervar comportamiento
-         expect( screen.getByText(clickFirstValue) ).toBeTruthy( );
-     });
- 
-     test('El botón "Click aquí" debe incrementar el valor de click', () => {
-         render( <Counter/> );
-         fireEvent.click( screen.getByRole('button', {name: 'Click aquí'}));
-         expect( screen.getByText(1) ).toBeTruthy( );
-     });
- 
-     test('El botón "Restaurar contador" debe poner el valor de click en 0', () => {
-         render( <Counter/> );
-         fireEvent.click( screen.getByRole('button', {name: 'Restaurar contador'}));
-         expect( screen.getByText(0) ).toBeTruthy( );
+     test('handleCategoryAdd debe modificar categories', () => {
+         expect( ).toBe( );
      });
  }) 
- 
- */
