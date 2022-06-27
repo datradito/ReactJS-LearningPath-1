372eff6ba0f4c1b51a9f26ea752be2dadcbe92e4
mariano ramos <nicom.ramos@hotmail.com>
2022-06-27 15:27:12 -0300

    CustomHooks - 180

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { Message } from './Message';
+
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'unknown',
+        email: 'nico@mail.com'
+    });
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
+        });
+    }
+
+    useEffect( () => {
+    }, [formState]);
+    
+
+    return (
+        <>
+            <h1>Formulario Simple</h1> 
+            <hr />
+
+            <input 
+                type="text" 
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={ username }
+                onChange={ onInputChange }
+            />
+
+            <input 
+                type="email" 
+                className="form-control mt-2"
+                placeholder="fernando@google.com"
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+            />
+
+
+            {
+                (username === 'strider2' ) && <Message />
+            }
+
+        </>
+    )
+}
