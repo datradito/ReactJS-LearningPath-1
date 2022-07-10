6a519ce7620922c6d144d43e128d138e4131537b
mariano ramos <nicom.ramos@hotmail.com>
2022-07-10 20:11:00 -0300

    Docs update

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,29 +1,22 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useForm } from '../hooks/useForm';
 
 export const AddCategory = ({ onNewCategory }) => {
-
-    const [ inputValue, setInputValue ] = useState('');
-
-    const onInputChange = ({ target }) => {
-        setInputValue( target.value );
-    }
+    const { formState, onInputChange } = useForm()
 
     const onSubmit = ( event ) => {
         event.preventDefault();
-        if( inputValue.trim().length <= 1) return;
-
-        // setCategories( categories => [ inputValue, ...categories ]);
-        setInputValue('');
-        onNewCategory( inputValue.trim() );
+        if( formState.search.trim().length <= 1 ) return;
+        onNewCategory( formState.search.trim() );
+        event.target[0].value = ""
     }
 
     return (
-        <form onSubmit={ onSubmit } aria-label="form">
+        <form className='form' onSubmit={ onSubmit } aria-label="form">
             <input 
                 type="text"
                 placeholder="Buscar gifs"
-                value={ inputValue }
+                name="search"
                 onChange={ onInputChange }
             />
         </form>
